65e65871b317ffba846d1f726375da9229b73650
Jason.K <vonhitl@gmail.com>
2019-03-19 14:15:00 +0200

    third commit

--- testpackage/__init__.py
+++ testpackage/__init__.py
@@ -1,2 +1,2 @@
-from testpackage.recursion import sum_array, fibonacci, factorial, reverse
-from testpackage.sorting import bubble_sort, merge_sort, quick_sort
+from . import recursion
+from . import sorting
